e175803718caf8c3cbc9fd5b447356a5242d2966
yxf <599367320@qq.com>
2020-06-30 01:59:01 +0800

    修改

--- src/axios.js
+++ src/axios.js
@@ -4,14 +4,14 @@ axios.defaults.baseURL = 'https://open.duyiedu.com' // 请求的根路径
 
 // 请求拦截器
 axios.interceptors.request.use(config => {
-    config.params = {
-        ...config.params,
-        appkey: 'love_sin_1593138646956'
-    }
-    return config
+  config.params = {
+    ...config.params,
+    appkey: 'love_sin_1593138646956'
+  }
+  return config
 }, error => {
-    // 对请求错误做些什么
-    return Promise.reject(error)
+  // 对请求错误做些什么
+  return Promise.reject(error)
 })
 
 export default axios
